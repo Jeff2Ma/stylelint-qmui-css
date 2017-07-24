1804313937bca2bdf06e44f424524d2662c3d660
Jeff2Ma <mjh2316@gmail.com>
2017-07-24 11:07:02 +0800

    Remove unused rules.

--- rules/index.js
+++ rules/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  'at-extend-no-missing-placeholder': require('./at-extend-no-missing-placeholder'),
-  "border-color-part-use-variable": require('./border-color-part-use-variable'),
+  // 'at-extend-no-missing-placeholder': require('./at-extend-no-missing-placeholder'),
+  // "border-color-part-use-variable": require('./border-color-part-use-variable'),
   "nested-qui-selector": require('./nested-qui-selector'),
   'selector-namespace-follow-filename': require('./selector-namespace-follow-filename'),
   'stylelint-disable-reason': require('./stylelint-disable-reason')
